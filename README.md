b1f2faeeb91398b117ca98832a6fc6c411e2232b
pragati57 <90565530+pragati57@users.noreply.github.com>
2023-11-06 00:26:06 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# Topsis-weblink
+Web app that generates the topsis score on your input .csv file.
+<h3> Active Link </h3>
+<h4><a href="http://topsisgenerator.pythonanywhere.com" target ="_blank" >Topsis</a></h4>
+
+<h3>Points to keep in your mind.</h3>
+1) Upload your .csv file.<br>
+2) Give the weights and impacts in accordance with the number of columns present in your .csv file.<br>
+3) Enter the mail id where you want to receive the result.
+
+<h4>To create your own webapp</h4>
+Here I used flask to integrate the python file with my .html files. You can try Django also. Download the above folders and files in your pc and add your own .html files to style the frontend.
+<br>
+<h3> See how it looks like: </h3>
+<img src="https://github.com/Mudrika29/Topsis-weblink/assets/72308910/013a5bd5-149c-4942-8b53-087c238739e4" height= 500px width=700px align=center>
+
--- func.py
+++ func.py
@@ -1,111 +1,110 @@
-import argparse
-import sys
-import os
-import numpy as np
 import pandas as pd
-from pandas.api.types import is_string_dtype
-from pandas.api.types import is_numeric_dtype
+import numpy as np
+import math
+import sys
+
+def topsis(dataframe,wt,impacts):
+
+    df=pd.read_csv(dataframe)
+    result=df.copy()
+    df=df.drop(df.columns[0],axis=1)
+    # wt=sys.argv[2].split(',')
+    weight=len(wt)
+    # impacts=sys.argv[3].split(',')
+    impact=len(impacts)
+    # output=sys.argv[4]
+    add_row=[]
+    ideal_best=[]
+    ideal_worst=[]
 
 
-def main(data,weights,impact):
-
-  df=pd.read_csv(data)
- 
-  if(len(weights) != len(impact)):
-        print("Error: Number of weights and number of impacts passed dont match")
+    if df.shape[1] <3:
+        print("Number of columns should be greater than 3")
         quit()
-  
-  for i in impact:
-      if i!='+' and i!='-':
-         print("Error: The impacts should be either '+'/'-'")
-         quit()
+
+    if weight!=impact:
+        print("Number of elements passed in weight is not equal to impacts")
+        quit()
+
+    if df.shape[1]!=weight:
+        print("Number of columns and weight/impact passed are not equal")
+        quit()
     
-  if(df.shape[1] <3):
-        print("Error: Number of columns should be >= 3")
-        quit()
-  if(df.shape[1]-1 != len(weights) or df.shape[1]-1 != len(impact)):
-        print("Error: Number of columns of df and number of weights/impacts passed dont match")
-        quit()
+    for i in impacts:
+        if i !='+' and i!='-':
+            print("Impact should be +/-")
+            quit()
 
-  
-  
-  result=df.copy()
-  ncol=len(df.columns)
-  
-  
- 
-  
-  curr=[]
-  for i in df.columns:
-      curr.append(i)
+    cat=[]
+    for i in df.columns:
+        cat.append(i)
+    cat.pop(0)
+    for i in cat:
+        if df[i].dtype!=np.int64 and df[i].dtype!=np.float64:
+            print("Data type should be numeric")
+            quit()
 
-  curr.pop(0)
-  for i in curr:
-      if df[i].dtype!=np.int64 and df[i].dtype != np.float64:
-        print("There is atleast one column with non-numeric data type.")
-        quit()
+    #Step1 Normalization
+    for i in range(len(df.axes[1])):
+        sum=0
+        for j in range(len(df.axes[0])):
+            sum+=df.iloc[j,i]**2
+        add_row.append(math.sqrt(sum))
+    # print(add_row)
 
-  
-  for i in range(1,ncol):
-      temp=0
+    # Weights normalization
+    for i in range(len(df.axes[1])):
+        for j in range(len(df.axes[0])):
+           df.iloc[j,i]= float(df.iloc[j,i])/float(add_row[i])
+           df.iloc[j,i]= float(df.iloc[j,i])*float(wt[i])
+
+    # Ideal best and ideal worst
+    high=[]
+    low=[]
+    for i in range(len(df.axes[1])):
+        high.append(df.iloc[:,i].max())
+        low.append(df.iloc[:,i].min())
+
+    for i in range(len(high)):
+        if impacts[i] == '+':
+            ideal_best.append(high[i])
+            ideal_worst.append(low[i])
+        else:
+            ideal_best.append(low[i])
+            ideal_worst.append(high[i])
+    # print(ideal_best)
+    # print(ideal_worst)
+    # Euclidean distance
+    S_best=[]
+    S_worst=[]
+    for i in range(len(df.axes[0])):
+        total_best=0
+        total_worst=0
+        for j in range(len(df.axes[1])):
+            total_best+=(df.iloc[i][j]-ideal_best[j])**2
+            total_worst+=(df.iloc[i][j]-ideal_worst[j])**2
+        S_best.append(total_best**0.5)
+        S_worst.append(total_worst**0.5)
+    # print(S_best)
+    # print(S_worst)
+
+    # Performance coloumn
+    Performance=[]
+    for i in range(len(S_best)):
+        Performance.append(float(S_worst[i])/float((S_best[i]+S_worst[i])))
+    # print(Performance)
     
-      for j in range(len(df)):
+    result['Performance']=Performance
+    result['Rank']=result['Performance'].rank(ascending=False).astype(int)
+    return result
+    # print(df)
+    # print(result)
+    # result.to_csv(output,index=False)
+    # print("Success")
+
+# topsis()
+
+
+    
         
-        temp=float(temp)+float(df.iloc[j,i])**2
-      
-      temp=temp**0.5
-      
 
-      for k in range(len(df)):
-         
-         df.iat[k,i]=(float(df.iloc[k,i])/float(temp))*float(weights[i-1])
-         
-
-  
-  p_val=(df.max().values)[1:]
-  n_val=(df.min().values)[1:]
-  
-  for i in range(1,ncol):
-      if impact[i-1]=='-':
-          p_val[i-1],n_val[i-1]=n_val[i-1],p_val[i-1]
-
-      
-
-  
-  S_p=[]
-  S_n=[]
-  for i in range(len(df)):
-      temp=0
-      temp1=0
-      for j in range(1,ncol):
-         temp=temp+(float(df.iloc[i,j])-float(p_val[j-1]))**2
-         temp1=temp1+(float(df.iloc[i,j])-float(n_val[j-1]))**2
-
-      temp=temp**0.5
-      temp1=temp1**0.5
-      S_p.append(temp)
-      S_n.append(temp1)
-      
-
-  
-  final=[]
-  for i in range(len(df)):
-       d=(float(S_n[i]))/(float(S_p[i])+float(S_n[i]))
-       final.append(d)
-
-  
-
-  result['Topsis score']=final
-  result['Rank']=result['Topsis score'].rank(ascending=False).astype(int)
-     
-#   result.to_csv(resultname,index=False)
-  return result
-
-  
-  
-
-
-
-    
-    
-    
